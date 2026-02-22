79b77cc16a7a9fce4a50fd108a136a5cda36de73
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2026-02-22 21:28:29 +0800

    chore: update gnark dependency to point to GitHub repository

--- cli/src/init/gnark.rs
+++ cli/src/init/gnark.rs
@@ -11,6 +11,6 @@ impl ProvingSystem for Gnark {
     const ADAPTER: Adapter = Adapter::Gnark;
 
     const DEPENDENCIES: &'static str = r#"
-rust-gnark = "0.0.1"
+rust-gnark = { git = "https://github.com/vivianjeng/rust-gnark", branch = "main" }
     "#;
 }
